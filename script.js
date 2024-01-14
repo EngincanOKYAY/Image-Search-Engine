2a0d28cd28834a5baaca788333d815fb7e7e54f7
engincanokyay <engincanokyay3535@gmail.com>
2024-01-14 16:37:03 +0300

    api will take

---
+++ script.js
@@ -0,0 +1,4 @@
+const searchForm = document.getElementById("search-form");
+const searchBoX = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
