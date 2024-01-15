eafcebd167591f7d00410f3bf47d2c7bbe1f9232
engincanokyay <engincanokyay3535@gmail.com>
2024-01-15 23:41:09 +0300

    debugging

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// 
-
 const accessKey = "cvbkoEp2z92lqvA51uwmAO6I5mspYnGyqW6FF27Z6aE";
 
 const searchForm = document.getElementById("search-form");
@@ -10,32 +8,20 @@ const showMoreBtn = document.getElementById("show-more-btn");
 let keyword = "";
 let page = 1;
 
-async function searchImages() {
-    keyword = searchBox.value ;
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
 
-    if (!keyword) {
-        console.error("Lütfen geçerli bir anahtar kelime girin.");
-        return;
-    }
+    const response = await fetch(url);
+    const data = await response.json();
 
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
-
-    try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error("API'den veri alınamadı.");
-        }
-
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error("Hata:", error.message);
-    }
+    console.log(data);
 }
 
-searchForm.addEventListener("submit", (e) => {
+searchForm.addEventListener("submit", (e)=>{
+
     e.preventDefault();
     page = 1;
     searchImages();
-});
+
+})
