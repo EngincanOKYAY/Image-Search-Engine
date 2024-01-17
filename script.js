85bd72f67214662100c241450ddad87bf1a4bef0
engincanokyay <engincanokyay3535@gmail.com>
2024-01-17 23:43:11 +0300

    API done

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-const accessKey = "cvbkoEp2z92lqvA51uwmAO6I5mspYnGyqW6FF27Z6aE";
+// const accessKey = "6bzQduWT6ceAskK3k0wJum3x6FZV6QVUrQ3z3nOzMJ0";
+
+// const searchForm = document.getElementById("search-form");
+// const searchBox = document.getElementById("search-box");
+// const searchResult = document.getElementById("search-result");
+// const showMoreBtn = document.getElementById("show-more-btn");
+
+// let keyword = "";
+// let page = 1;
+
+// async function searchImages(){
+//     keyword = searchBox.value;
+//     const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     const results = data.results;
+
+//     results.map((result) => {
+//         const image =document.createElement("img");
+//         image.src = result.urls.small;
+//         const imageLink =document.createElement("a");
+//         imageLink.href = result.links.html;
+//         imageLink.target="_blank";
+
+//         imageLink.appendChild(image);
+//         searchResult.appendChild(imageLink);
+//     })
+// }
+
+// searchForm.addEventListener("submit", (e)=>{
+
+//     e.preventDefault();
+//     page = 1;
+//     searchImages();
+
+// })
+
+const accessKey = "6bzQduWT6ceAskK3k0wJum3x6FZV6QVUrQ3z3nOzMJ0";
 
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
@@ -8,20 +47,32 @@ const showMoreBtn = document.getElementById("show-more-btn");
 let keyword = "";
 let page = 1;
 
-async function searchImages(){
+async function searchImages() {
     keyword = searchBox.value;
-    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    const results = data.results;
+
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    });
 }
 
-searchForm.addEventListener("submit", (e)=>{
-
+searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    // Clear previous search results
+    searchResult.innerHTML = "";
     page = 1;
     searchImages();
-
-})
+});
